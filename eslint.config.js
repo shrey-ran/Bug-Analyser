bcd885f0a61eba25893d5fd1c36c5cf692af5d2e
Shreyansh Ranjan <shrey.ran@example.com>
2026-02-08 12:44:58 +0530

    Fix ESLint for React/JSX files

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,25 @@
 import js from '@eslint/js';
 import globals from 'globals';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import { fixupPluginRules } from '@eslint/compat';
 
 export default [
   js.configs.recommended,
   {
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      react,
+      'react-hooks': fixupPluginRules(reactHooks),
+    },
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: {
         ...globals.browser,
         ...globals.node,
@@ -14,10 +27,20 @@ export default [
         ...globals.jest,
       },
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     rules: {
       'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
       'no-console': 'off',
       'no-control-regex': 'warn',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
     },
   },
   {
